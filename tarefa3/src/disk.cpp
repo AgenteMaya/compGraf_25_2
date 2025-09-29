a9685e71a611fe508f14fcc9d728c9599a834f2f
Mayara <rrddamazio@gmail.com>
2025-09-29 12:57:22 -0300

    WIP FUNCIONA POR FAVOR

---
+++ tarefa3/src/disk.cpp
@@ -0,0 +1,43 @@
+#include "disk.h"
+
+#include <iostream>
+
+#ifdef _WIN32
+#include <glad/glad.h>
+
+#elif __APPLE__
+#include <OpenGL/gl3.h>
+
+#elif __linux__
+#include <glad/glad.h>
+#endif
+
+DiskPtr Disk::Make (int nslice)
+{
+  return DiskPtr(new Disk(nslice));
+}
+
+Disk::Disk (int nslice) : m_nslice(nslice)
+{
+  float coord[] = {-1.0f,0.0f,1.0f,0.0f,0.0f,1.0f};
+  // create VAO
+  glGenVertexArrays(1,&m_vao);
+  glBindVertexArray(m_vao);
+  // create coord buffer
+  GLuint id;
+  glGenBuffers(1,&id);
+  glBindBuffer(GL_ARRAY_BUFFER,id);
+  glBufferData(GL_ARRAY_BUFFER,sizeof(coord),coord,GL_STATIC_DRAW);
+  glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,0,0);  // coord
+  glEnableVertexAttribArray(0);
+}
+
+Disk::~Disk () 
+{
+}
+
+void Disk::Draw (StatePtr )
+{
+  glBindVertexArray(m_vao);
+  glDrawArrays(GL_TRIANGLES,0,3);
+}
